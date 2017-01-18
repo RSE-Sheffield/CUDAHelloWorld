2eab56f1d535c7892cd0bc49523798ad54bc1286
mondus <p.richmond@sheffield.ac.uk>
2017-01-18 16:02:20 +0000

    Example updated

---
+++ helloworld.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Request GPU resource
+#$ -l gpu-1
+# Use the flybrain project group
+#$ -P flybrain
+#Use the flybrain queue
+#$ -q flybrain.q 
+
+# Call the hello world executable
+./helloworld
