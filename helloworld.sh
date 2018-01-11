396c6037c936b178882a58d4a50c544ef9a1bc7d
mondus <p.richmond@sheffield.ac.uk>
2018-01-11 13:54:28 +0000

    updated training queue

--- helloworld.sh
+++ helloworld.sh
@@ -2,9 +2,9 @@
 # Request GPU resource
 #$ -l gpu=1
 # Use the training project group
-#$ -P training
+#$ -P rse-training
 #Use the training queue
-#$ -q training.q 
+#$ -q rse-training.q 
 
 # Call the hello world executable
 ./helloworld
